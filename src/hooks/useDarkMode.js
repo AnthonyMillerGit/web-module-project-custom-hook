8e58b57209d60f8c47a0e04349d7459d97c35a47
ferengitooth <ehitsamill@gmail.com>
2021-07-09 09:35:00 -0700

    hit mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from "./useLocalStorage"
+
+
+export const useDarkMode = (initialValue) => {
+    const [value, setValue] = useLocalStorage('darkmode', initialValue);
+
+    return [value, setValue];
+}
+
